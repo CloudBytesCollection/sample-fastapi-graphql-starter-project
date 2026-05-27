8d2d5073517bc1141d7d3ba9a43e61db9f1392f7
Eidan Rosado <edy.vision18@gmail.com>
2026-05-27 13:12:07 -0600

    Archive Repo

--- app/resolvers/hello/resolve_hello.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-from app.models.hello.hello_response_models \
-    import HelloResponse
-
-
-async def resolve_hello_world() -> HelloResponse:
-    return HelloResponse("Hello World!", datetime.datetime.now())
